82c04b705ef7e93e7bca3e3ccf5ed8f8ace34245
Dinesh Pal <dineshpaldinesh3@gmail.com>
2019-12-09 17:05:04 +0530

    complete codeigniter multiple choice question quiz project

---
+++ application/views/quiz_game.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Quiz_Game</title>
+
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to Quiz!</h1>
+
+<form method="" action="<?php echo base_url('Questions/quizdisplay');?>">
+	<input type="submit" value="start">
+</form>
+	
+</div>
+
+</body>
+</html>
